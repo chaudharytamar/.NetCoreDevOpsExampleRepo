c8e6ecd16e1381a7a7578736efa9de96234f28e9
manvendra <chaudharytamar@gmail.com>
2026-03-08 09:14:26 +0530

    Initial Commit:.Net Core Project Setup

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace NetCoreDevOpsExample.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
